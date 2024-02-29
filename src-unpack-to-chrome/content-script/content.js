c09745bba908d09f326c0268ff3a5ad6d5db2fd8
kairos310 <kairos0319@gmail.com>
2024-02-29 16:35:20 -0600

    moved content.js to content-script folder, renamed the higlighting one, updated manifest

--- src-unpack-to-chrome/content-script/content.js
+++ src-unpack-to-chrome/content-script/content.js
@@ -1,286 +1,73 @@
-// TODO: highlight all text before anything 
-
-let highlight_applied = false;
-// set this to true before applying the newest highlight
-
-let highlight = {
-  "url":"https",
-  "scroll": 0,
-  "headindex": 0,
-  "tailindex": 0
+var doc = ""
+var test = [document.getElementsByTagName('p'),
+// document.getElementsByTagName('span')
+]
+// test.append(document.getElementsByTagName('span'))
+for (thing of test) {
+  for (t of thing) {
+    doc += t.innerText
+  }
 }
 
-let pageHighlights = []
-
-let color_block = `
-  <input id="%1" type="radio" name="choice" value="%2">     
-  <label for="%1" class="color_label">
-    %3
-  </label>
-`
-
-window.addEventListener("keydown", (e)=>{
-    if (document.getElementsByClassName('readfxpopup').length > 0){
-        document.getElementsByClassName('readfxpopup')[0].remove()
-    }
-})
-
-
-chrome.runtime.onMessage.addListener(({ name, data }) => {
-  if(name === 'show_highlights'){
-    console.log("show highlights")
-    highlight_applied = true
-    construct_range(data)
-  }
-  if(name === 'jump_to_highlights'){
-    console.log("jump to highlights")
-    jump_to_highlights(data)
-  }
-})
-
-
-async function add_new_highlight(e){
-  if(!highlight_applied){
-    // await chrome.runtime.sendMessage(()=>{
-
-    // })
-  }
-  const regex = new RegExp(/%(\d)/, 'g')
-  element = window.getSelection().focusNode.parentNode;  
-  
-  let popup = document.createElement("div");
-  popup.innerHTML = "Highlight <br>"
-  for(let i = 0; i < 4; i++){
-      popup.innerHTML += color_block.replaceAll(regex, "color" + i)
-  }
-  popup.style.top = e.clientY + "px";
-  popup.style.left = e.clientX + "px";
-  popup.classList.add("readfxpopup")
-  document.body.appendChild(popup)
-
-  let selection = window.getSelection()
-  let range = selection.getRangeAt(0)
-
-  highlight = save_range(range)
-  
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function bold_text(word, text_elements) {
 
-window.addEventListener("mouseup", (e)=>{
-  
-    if(e.target.classList[0] == "color_label"){
-      
-      chrome.runtime.sendMessage({
-        name: 'highlight_text',
-        data: highlight
-      })
+  words = word.split("/")
+  if (words.length < 2) {
+    return
+  }
+  // catch errors
 
-      construct_range([highlight])
-    }
+  //text_elements = document.getElementsByTagName('p') + document.getElementsByTagName('span')
+  // text_elements = [
+  //   document.getElementsByTagName('p'),
+  //   // document.getElementsByTagName('h1'),
+  //   // document.getElementsByTagName('h2'),
+  //   // document.getElementsByTagName('h3'),
+  //   // document.getElementsByTagName('h4'),
+  //   // document.getElementsByTagName('h5'),
+  //   // document.getElementsByTagName('span')
+  //   //document.getElementsByTagName('div')
+  // ]
 
-    if (document.getElementsByClassName('readfxpopup').length > 0){
-        document.getElementsByClassName('readfxpopup')[0].remove()
-    }
-    if (window.getSelection().toString() != "") {
-        if(!highlight_applied){
-          console.log("enable show highlights first")
-          //return
+  // console.log(wordlist.length)
+
+      for (ele of text_elements) {
+        for(p of ele){
+            for (var i = 0; i < words.length && i < 500; i ++) {
+              if (words[i] == "") {
+                continue
+              }
+              try {
+                re = "( " + words[i] + " )"
+                re = new RegExp(re, 'gi')
+                p.innerHTML = p.innerHTML.replaceAll(re, "<b>$1</b>")
+          }
+          catch(error) {
+            continue
+          }
         }
-        add_new_highlight(e)
-    } 
-    // else if (document.selection && document.selection.type != "Control") {
-    //     text = document.selection;
-    // }
-})
-
-function highlight_text_node(n, start, end){
-  let node = n
-  let htext, text, ttext;
-
-  // If is element
-  if(node.nodeType == 1){
-    htext = node.innerHTML.substring(0, start);
-    text = node.innerHTML.substring(start, end);
-    ttext = node.innerHTML.substring(end); 
-
-    // If selection is to end
-    if(end == -1){
-      text = node.innerHTML.substring(start);
-      ttext = "";
-    }
-    console.log("node: " + text)
-    node.innerHTML = htext +  "<highlight-tag>" + text + "</highlight-tag>" + ttext
-    return
-  }
-  
-  // if text node
-  if(node.nodeType == 3){
-    // Get element
-    let parent = node.parentNode;
-    htext = node.data.substring(0, start);
-    text = node.data.substring(start, end);
-    ttext = node.data.substring(end); 
-    
-    if(end == -1){
-      text = node.data.substring(start);
-      ttext = "";
-    }
-    let eleindex = 0;
-    try{
-      parent.childNodes
-    }catch{
-      console.log(parent, node)
-    }
-    parent.childNodes.forEach((n,i)=>{
-        if (n.isSameNode(node)) eleindex = i
-    })
-    //console.log(eleindex)
-
-    let headElement = document.createTextNode(htext);
-    let markElement = document.createElement('highlight-tag');
-    let tailElement = document.createTextNode(ttext);
-
-    markElement.classList.add("highlight_text")
-    markElement.innerHTML = text;
-
-    parent.removeChild(parent.childNodes[eleindex])
-    parent.insertBefore(tailElement, parent.childNodes[eleindex]);
-    parent.insertBefore(markElement, parent.childNodes[eleindex]);
-    parent.insertBefore(headElement, parent.childNodes[eleindex]);
-    return
-  }
-}
-
-function save_range(range){
-  // let highlight = range;
-  // return highlight
-  let highlight = {
-    url: window.location.href,
-    headtype: range.startContainer.nodeType,
-    tailtype: range.endContainer.nodeType,
-    headindex: range.startOffset,
-    tailindex: range.endOffset,
-    scroll: window.scrollY
-  }
-  if (highlight.headtype == 3){
-    highlight.headnode = jsPath(range.startContainer.parentElement)
-    let nodeindex = 0;
-    let p = range.startContainer.parentElement.childNodes;
-    for(let i = 0; i < p.length; i++){
-      if(p[i].isSameNode(range.startContainer)){
-        nodeindex = i  
+        await sleep(100)
       }
-    }
-    highlight.nodeindex = nodeindex
-    highlight.text = range.startContainer.data.substring(highlight.headindex)
-  }
-  if (highlight.tailtype == 3){
-    let nodeindex = 0;
-    let p = range.endContainer.parentElement.childNodes;
-    for(let i = 0; i < p.length; i++){
-      if(p[i].isSameNode(range.endContainer)){
-        nodeindex = i  
-      }
-    }
-    highlight.nodeindex = nodeindex
-    highlight.tailnode = jsPath(range.endContainer.parentElement)
-  }
-    
-  if (highlight.tailtype == 1){
-    highlight.tailnode = jsPath(range.endContainer)
-    highlight.text = range.startContainer.innerText.substring(highlight.headindex)
-  }
-  if (highlight.headtype == 1){
-    highlight.headnode = jsPath(range.startContainer)
-  }
-  
-  console.log(highlight)
-  return highlight
+
+}
 }
 
-function highlight_all(){
-  let htmlselection = window.getSelection()
-  let range = htmlselection.getRangeAt(0)
-  let range_save = range.cloneRange()
-  save_range(range_save)
-  let extent = htmlselection.focusNode
-  let anchor = htmlselection.anchorNode;
-  let startindex = htmlselection.anchorOffset
-  let endindex = htmlselection.focusOffset
 
-  // Within single element
-  if(extent.isSameNode(anchor)){
-    highlight_text_node(anchor, startindex, endindex)
-    return
+console.log(doc)
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+  if (msg == 'get_document') {
+    sendResponse(doc)
   }
-  highlight_text_node(anchor, startindex, -1)
-  highlight_text_node(extent, 0, endindex)
 
-  if(range.startContainer.nextSibling == null){
-    range.setStartAfter(range.startContainer.parentElement.nextElementSibling)    
-    range.setEndBefore(range.endContainer.parentElement.previousElementSibling)    
-  }else{
-    range.setStartAfter(range.startContainer.nextSibling)
-    range.setEndBefore(range.endContainer.previousSibling)
+  if (msg[0] == "extract keywords") {
+    // for (t of test) {
+    //   for (p of t){
+    bold_text(msg[1], test)
+  // }
+  // }
   }
-  fullnodes = range.extractContents().querySelectorAll('*');
-  head = fullnodes.item(0).cloneNode();
-  tail = fullnodes.item(fullnodes.length - 1).cloneNode();
-  parentnode = document.createDocumentFragment();
-  for (let i = 0; i < fullnodes.length; i++){
-    fullnodes.item(i).innerHTML = "<mark>" + fullnodes.item(i).innerHTML + "</mark>"
-    console.log(fullnodes.item(i))
-    parentnode.appendChild(fullnodes.item(i))    
-  }
-  range.insertNode(parentnode)
-}
-
-async function jump_to_highlights(h){
-  let headele = document.querySelector(h.headnode)
-  construct_range([h])
-  //await window.open(h.url, "_blank")
-  headele.scrollIntoView()
-}
-
-function construct_range(highlights){
-  highlights.forEach((h)=>{
-    console.log(h.url, window.location.href)
-    if(h.url == window.location.href){
-      console.log(h.url)
-      let range = document.createRange()
-      let headele = document.querySelector(h.headnode)
-      let tailele = document.querySelector(h.tailnode)
-
-      console.log(headele)
-      console.log(h.headindex, h.tailindex)
-      if (h.headtype == 3){
-        try {
-          range.setStart(headele.childNodes[h.nodeindex], h.headindex)
-        } catch (e) {
-          console.log(headele)
-          console.log(h)
-          console.error(e);
-        }
-
-        //range.setStart(headele.childNodes[0], h.headindex)
-      }else{
-        range.setStart(headele, h.headindex)
-      }
-      if (h.tailtype == 3){
-        range.setEnd(tailele.childNodes[h.nodeindex], h.tailindex)
-      }else{
-      range.setEnd(tailele, h.tailindex)
-
-
-      }selection = window.getSelection()
-      if (selection.rangeCount > 0) {
-        selection.removeAllRanges();
-      }    
-      selection.addRange(range)
-      highlight_all()  
-    }
-    
-    
-  })
-}
-
+});
