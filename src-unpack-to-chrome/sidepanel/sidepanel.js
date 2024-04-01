102b8c1f5f051b8e628c40257fb050cae1da881d
kairos310 <kairos0319@gmail.com>
2024-03-31 20:21:15 -0500

    fixed lots of highlighting edge cases, especially in wikipedia

--- src-unpack-to-chrome/sidepanel/sidepanel.js
+++ src-unpack-to-chrome/sidepanel/sidepanel.js
@@ -64,6 +64,7 @@ async function jump_to_highlight(h){
         console.log(tabId)
         console.log(newtab.id)    
         if (tabId == newtab.id && changeInfo.status == 'complete') {
+            showHighlights()
             let response = chrome.tabs.sendMessage(newtab.id, { name: 'jump_to_highlights', data: h});
         }
     });
@@ -94,6 +95,8 @@ function deletehighlight(title){
 }
 
 function listHighlights(){
+    let list = document.getElementById("highlight-links");
+    list.innerHTML = ""
 
     chrome.storage.local.get(["highlights"]).then((result)=>{
         for(let w in result.highlights){
@@ -113,29 +116,11 @@ function listHighlights(){
                 link_ele.addEventListener('click', ()=>{
                     jump_to_highlight(h)
                 })
-                document.getElementById("highlight-links").appendChild(link_ele)
+                list.appendChild(link_ele)
 
                
             }
         }
-        // result.highlights.map((h)=>{
-        //     let link_ele = document.createElement('div');
-        //     const regex = /(?<=https:\/\/)[a-z.]+(?=\/)/gm;
-        //     link_ele.innerHTML = "<a href='" + h.url + "'>" + regex.exec(h.url) + "</a>" + "<br>" + h.text;
-        //     let button = document.createElement("button");
-        //     button.innerText = "X"
-        //     button.classList.add("delete-button");
-        //     button.addEventListener("click", ()=>{
-        //         deletehighlight(h)
-        //     })
-        //     link_ele.appendChild(button);
-        //     link_ele.classList.add('highlight-link');
-        //     link_ele.addEventListener('click', ()=>{
-        //         jump_to_highlight(h)
-        //     })
-        //     document.getElementById("highlight-links").appendChild(link_ele)
-
-        // })
     })
 }
 
