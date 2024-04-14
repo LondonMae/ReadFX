eece544a96c04dfa476415b2373748232deea537
London Bielicke <bielm-24@rhodes.edu>
2024-04-14 09:07:40 -0500

    search bar

--- src-unpack-to-chrome/sidepanel/sidepanel.js
+++ src-unpack-to-chrome/sidepanel/sidepanel.js
@@ -222,7 +222,7 @@ function cleanPage(){
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    var openReadingModeButton = document.getElementById('open-reading-mode-button');
+    var openReadingModeButton = document.getElementById('read-button');
     openReadingModeButton.addEventListener('click', function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             var activeTab = tabs[0];
@@ -242,28 +242,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-document.getElementById("read-button").addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        console.log("Button Clicked");
-        const tab = tabs[0];
-        function getParas() {
-            var paragraphs = document.getElementsByTagName("p");
-            var textCollection = [];
-
-            for (var i = 0; i < paragraphs.length; i++) {
-                textCollection.push(paragraphs[i].textContent);
-            }
-
-            console.log(textCollection);
-        };
-
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            func: getParas,
-            //        files: ['contentScript.js'],  // To call external file instead
-        }).then(() => console.log('Injected a function!'));
-    });
-});
 
 document.getElementById("bold-button").addEventListener("click", () => {
   console.log("bolding");
