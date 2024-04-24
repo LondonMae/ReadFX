8d08fd203980dd810275df654434cdd2aedcb540
albert <albert.ng.hoang@gmail.com>
2024-04-24 14:20:53 -0500

    Added DETLETE func

--- db_container/app.py
+++ db_container/app.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, jsonify
 from db import create_connection, create_database, create_tables
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-
 # Create a new user
-@app.route('/users', methods=['POST'])
+@app.route("/users", methods=["POST"])
 def create_user():
     connection = create_connection()
     if connection is None:
@@ -24,8 +23,9 @@ def create_user():
         if connection:
             connection.close()
 
+
 # Get all users
-@app.route('/users', methods=['GET'])
+@app.route("/users", methods=["GET"])
 def get_users():
     connection = create_connection()
     if connection is None:
@@ -42,8 +42,9 @@ def get_users():
         if connection:
             connection.close()
 
+
 # Create a note
-@app.route('/notes', methods=['POST'])
+@app.route("/notes", methods=["POST"])
 def create_notes():
     connection = create_connection()
     print("Creating Notes")
@@ -54,22 +55,28 @@ def create_notes():
 
         # Extract data from the request
         data = request.get_json()
-        user_id = data.get('user_id')  # Assuming you pass user_id in the request
-        note_header = data.get('note_header')
-        note_content = data.get('note_content')
+        user_id = data.get("user_id")  # Assuming you pass user_id in the request
+        note_header = data.get("note_header")
+        note_content = data.get("note_content")
 
         # Check if user_id exists in the table
-        cursor.execute('''
+        cursor.execute(
+            """
             INSERT INTO Users (user_id)
             VALUES (%s)
             ON CONFLICT (user_id) DO NOTHING
-        ''', (user_id,))
+        """,
+            (user_id,),
+        )
 
         # Insert the data into the Notes table
-        cursor.execute('''
+        cursor.execute(
+            """
             INSERT INTO Notes (user_id, note_header,note_content)
             VALUES (%s, %s, %s)
-        ''', (user_id, note_header, note_content))
+        """,
+            (user_id, note_header, note_content),
+        )
 
         # Commit the transaction
         connection.commit()
@@ -84,8 +91,39 @@ def create_notes():
         if connection:
             connection.close()
 
+
+@app.route("/users/<int:user_id>/notes/<string:note_header>", methods=["DELETE"])
+def remote_user_notes(user_id, note_header):
+    print("Deleting notes from user")
+    connection = create_connection()
+    if connection is None:
+        return jsonify({"error": "Database connection error"}), 500
+    try:
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            DELETE FROM Notes WHERE user_id = %s AND note_header = %s
+
+            """,
+            (user_id, note_header),
+        )
+        connection.commit()  # Committing the changes
+        if cursor.rowcount == 0:
+            return (
+                jsonify({"error": "No notes found for the given user and header"}),
+                404,
+            )
+        return jsonify({"success": "Notes deleted"}), 200
+    except Exception as e:
+        connection.rollback()  # Roll back in case of error
+        return jsonify({"error": str(e)}), 500
+    finally:
+        cursor.close()
+        connection.close()
+
+
 # Route to retrieve all notes belonging to a user
-@app.route('/users/<int:user_id>/notes', methods=['GET'])
+@app.route("/users/<int:user_id>/notes", methods=["GET"])
 def get_user_notes(user_id):
     print("Gettings notes")
     connection = create_connection()
@@ -95,19 +133,26 @@ def get_user_notes(user_id):
         cursor = connection.cursor()
 
         # Fetch notes belonging to the user
-        cursor.execute('''
+        cursor.execute(
+            """
             SELECT * FROM Notes WHERE user_id = %s
-        ''', (user_id,))
+        """,
+            (user_id,),
+        )
         notes = cursor.fetchall()
 
-        notes_list = [{"user_id": note[1], "header": note[2], "content": note[3]} for note in notes]
+        notes_list = [
+            {"user_id": note[1], "header": note[2], "content": note[3]}
+            for note in notes
+        ]
         return jsonify(notes_list), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     finally:
         if connection:
             connection.close()
-            
+
+
 @app.before_first_request
 def before_first_request_func():
     create_database()
@@ -117,12 +162,12 @@ def before_first_request_func():
         create_tables(connection)
         connection.close()
 
-@app.route('/')
-def home():
-    return {"message" : "This is root dir, use /users or /notes to interact with notes"}
 
+@app.route("/")
+def home():
+    return {"message": "This is root dir, use /users or /notes to interact with notes"}
 
 
 # Main function
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
